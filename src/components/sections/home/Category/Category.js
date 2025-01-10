69332dea6b01c7b1b23bb381a788a0f2a6a17c05
Divyeshrzeusint <divyeshr@zeusint.com>
2025-01-10 06:45:24 +0530

    set price product section accroding to app

--- src/components/sections/home/Category/Category.js
+++ src/components/sections/home/Category/Category.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Category.css";
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import { Tab, Nav } from "react-bootstrap";
 
 const Category = () => {
   const history = useHistory();
@@ -17,29 +18,53 @@ const Category = () => {
   }
 
   return (
-    <div className="categories-box-layout">
+    <section className="restaurant-tab-area">
       <div className="container">
-        <div className="categories-box-layout-inner">
-          <div className="row">
-            {categoryposts.map((item, i) => (
-              <div key={i} className="col-lg-3 col-sm-6">
-                  <Link onClick={() => handleNavigation("/classification", item)} className="categories-box">
-                      <span className="icon" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                        <i className={item.icon} />
-                      </span>
-                      <h5 className="title">{item.name}</h5>
-                      {/* <p>{item.numberofproduct} Products</p> */}
-                      <span className="overlay-icon">
-                          <i className={item.icon} />
-                      </span>
-                  </Link>
-              </div>
-            ))}
-          </div>
-        </div>
+        <Tab.Container defaultActiveKey="earrings">
+          <Nav
+            variant="pills"
+            className="restaurant-rood-list row justify-content-center mb-30"
+          >
+            {categoryposts?.map((item) => {
+              return (
+                <Nav.Item className="col-lg-2 col-md-3 col-sm-4 col-6" onClick={() => handleNavigation("/classification", item)}>
+                  <Nav.Link eventKey={item?.name}>
+                    <i className={item?.icon} />
+                    <span className="title">{item?.name}</span>
+                  </Nav.Link>
+                </Nav.Item>
+              )
+            })}
+          </Nav>
+        </Tab.Container>
       </div>
-    </div>
-  );
+    </section>
+  )
+
+  // return (
+  //   <div className="categories-box-layout">
+  //     <div className="container">
+  //       <div className="categories-box-layout-inner">
+  //         <div className="row">
+  //           {categoryposts.map((item, i) => (
+  //             <div key={i} className="col-lg-3 col-sm-6">
+  //                 <Link onClick={() => handleNavigation("/classification", item)} className="categories-box">
+  //                     <span className="icon" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+  //                       <i className={item.icon} />
+  //                     </span>
+  //                     <h5 className="title">{item.name}</h5>
+  //                     {/* <p>{item.numberofproduct} Products</p> */}
+  //                     <span className="overlay-icon">
+  //                         <i className={item.icon} />
+  //                     </span>
+  //                 </Link>
+  //             </div>
+  //           ))}
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Category;
